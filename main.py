7ec0a1cfb943ae086e485922b9e24df4c89e539c
nyasoo <gen33482@gmail.com>
2026-05-16 14:31:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ async def end(interaction: discord.Interaction):
     )
 
 @bot.tree.command(name="invite",description="指定した人をVCに招待")
-async def invite(interaction: discord.Interaction, user:mention):
+async def invite(interaction: discord.Interaction, user:discord.mention):
 
     # 自分のVC取得
     channel = private_rooms.get(
