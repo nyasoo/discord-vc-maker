40bbcc9d1c59a5d1109c507a1bed021d0555007e
nyasoo <gen33482@gmail.com>
2026-05-16 13:58:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from discord.ext import commands
 
 private_rooms = {}
 
+intents = discord.Intents.default()
+
+intents.members = True
+
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
